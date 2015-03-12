3949571671ab552fd49cab5414d13a8afa930975
zhengcano <zhengcano@gmail.com>
2015-03-12 16:32:37 -0700

    add gulp script to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -137,6 +137,14 @@ fi
 #   exitWithMessageOnError "grunt failed"
 #   cd - > /dev/null
 # fi
+
+# 6. Run gulp
+  if [ -e "$DEPLOYMENT_TARGET/gulpfile.js" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  ./node_modules/.bin/gulp gulp production
+  exitWithMessageOnError "gulp failed"
+  cd - > /dev/null
+fi
 ##################################################################################################################################
 
 # Post deployment stub
