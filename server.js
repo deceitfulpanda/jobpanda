a3eb84c7a7f25dc791f788850f3c901e009f6795
zhengcano <zhengcano@gmail.com>
2015-03-12 18:12:03 -0700

    fix Console.log to console.log

--- server.js
+++ server.js
@@ -1,30 +1,4 @@
-// var app = require('./server/server-config.js');
-
-/*==================== REQUIRE DEPENDENCIES ====================*/
-var express     = require('express'),
-    bodyParser  = require('body-parser'),
-    passport    = require('passport'),
-    http        = require('http');
-
-/*===================== INITIALIZE EXPRESS =====================*/
-var app = express();
-
-/*===================== INITIALIZE ROUTERS =====================*/
-// var userRouter = express.Router();
-// var listingRouter = express.Router();
-
-/*================== CONFIGURE EXPRESS MODULES =================*/
-// app.configure(function(){
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/client/dist'));
-// });
-
-// app.get('/', function(req, res){
-// 	console.log('hi');
-// 	res.send('hi');
-// })
-
+var app = require('./server/server-config.js');
 
 var port = process.env.PORT || 8000;
 
