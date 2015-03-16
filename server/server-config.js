16daf380d67451bd6682efa954f6797d469625ad
zhengcano <zhengcano@gmail.com>
2015-03-16 13:21:42 -0700

    Allow CORS in Express

--- server/server-config.js
+++ server/server-config.js
@@ -7,6 +7,12 @@ var express     = require('express'),
 /*===================== INITIALIZE EXPRESS =====================*/
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /*===================== INITIALIZE ROUTERS =====================*/
 // var userRouter = express.Router();
 // var listingRouter = express.Router();
